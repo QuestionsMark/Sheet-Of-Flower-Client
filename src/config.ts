8079ab0f16136e9121de6c7a63f16d5558940a64
Sławomir Dziurman <g1.slawomir.dziurman@gmail.com>
2022-05-22 12:26:08 +0200

    prepare for deploy

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
-// export const HOST_ADDRESS = 'http://localhost:9000';
-export const HOST_ADDRESS = "https://sheet-of-flower.herokuapp.com";
+// export const HOST_ADDRESS = process.env.REACT_APP_API_URL ?? "http://localhost:9000";
+export const HOST_ADDRESS = "https://sheet-of-flower.herokuapp.com"
