742f1ad3b73e31a4253347d087aa84d3f3d5c12e
Sławomir Dziurman <g1.slawomir.dziurman@gmail.com>
2022-05-24 11:27:33 +0200

    useLocalStorage hook added

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,4 @@
-// export const HOST_ADDRESS = process.env.REACT_APP_API_URL ?? "http://localhost:9000";
-export const HOST_ADDRESS = "https://sheet-of-flower.herokuapp.com"
+export const HOST_ADDRESS = process.env.REACT_APP_API_URL ?? "http://localhost:9000";
+// export const HOST_ADDRESS = "https://oela.networkmanager.pl";
+
+export const LOCAL_STORAGE_PREFIX = 'oela-';
